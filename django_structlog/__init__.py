8e385a61118c95dada4f626a7e5cecd8cbe4352a
Jules Robichaud-Gagnon <jules.robichaud@solutionstlm.com>
2019-04-08 18:30:57 -0400

    Add more setup

--- django_structlog/__init__.py
+++ django_structlog/__init__.py
@@ -1,1 +1,2 @@
 default_app_config = 'django_structlog.apps.DjangoStructLogConfig'
+name = "django_structlog"
--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 
 setup(
@@ -7,6 +11,8 @@ setup(
     author='Jules Robichaud-Gagnon',
     author_email='j.robichaudg+pypi@gmail.com',
     description='For using structlog in Django',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/plumdog/django_migration_testcase',
     packages=[
         'django_structlog',
