9461e4b852e255332c18b869ae6cd731ce7f69e7
Jules Robichaud-Gagnon <jules.robichaud@solutionstlm.com>
2019-04-10 00:08:34 -0400

    Complete coverage

---
+++ django_structlog_demo_project/users/tests/test_adapters.py
@@ -0,0 +1,13 @@
+import pytest
+
+from django_structlog_demo_project.users.adapters import SocialAccountAdapter, AccountAdapter
+
+pytestmark = pytest.mark.django_db
+
+
+class TestUserCreationForm:
+    def test_account_adapter(self):
+        assert AccountAdapter().is_open_for_signup(None)
+
+    def test_social_account_adapter(self):
+        assert SocialAccountAdapter().is_open_for_signup(None, None)
