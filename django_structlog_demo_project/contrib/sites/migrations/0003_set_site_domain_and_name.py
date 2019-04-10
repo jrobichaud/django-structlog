e7e244226fc21c25d73c93cf564dc40f0c399adf
Jules Robichaud-Gagnon <jules.robichaud@solutionstlm.com>
2019-04-10 00:02:18 -0400

    Remove unnecessary migration

--- django_structlog_demo_project/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ django_structlog_demo_project/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -19,16 +19,8 @@ def update_site_forward(apps, schema_editor):
     )
 
 
-def update_site_backward(apps, schema_editor):
-    """Revert site domain and name to default."""
-    Site = apps.get_model("sites", "Site")
-    Site.objects.update_or_create(
-        id=settings.SITE_ID, defaults={"domain": "example.com", "name": "example.com"}
-    )
-
-
 class Migration(migrations.Migration):
 
     dependencies = [("sites", "0002_alter_domain_unique")]
 
-    operations = [migrations.RunPython(update_site_forward, update_site_backward)]
+    operations = [migrations.RunPython(update_site_forward, migrations.RunPython.noop)]
