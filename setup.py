4faa00e7518d3076589b740d979e538b63796822
Jules Robichaud-Gagnon <jules.robichaud@solutionstlm.com>
2019-04-21 11:27:59 -0400

    Add logging topic

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: System :: Logging",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ]
