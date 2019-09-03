6b05e47a525b34cb825a3c0a61f3d2a66f40571e
Paul Parker <paul.parker@blinkhealth.com>
2019-09-03 11:40:55 -0400

    Provide default broker url

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,6 +3,7 @@ Base settings to build other settings files upon.
 """
 
 import environ
+from django.core.exceptions import ImproperlyConfigured
 
 ROOT_DIR = (
     environ.Path(__file__) - 3
@@ -217,7 +218,13 @@ if USE_TZ:
     # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-timezone
     CELERY_TIMEZONE = TIME_ZONE
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_url
-CELERY_BROKER_URL = env("CELERY_BROKER_URL")
+
+try:
+    CELERY_BROKER_URL = env("CELERY_BROKER_URL")
+    print(CELERY_BROKER_URL)
+# This breaks when running tests locally.
+except ImproperlyConfigured:
+    CELERY_BROKER_URL = "redis://0.0.0.0:6379/0"
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_backend
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-accept_content
