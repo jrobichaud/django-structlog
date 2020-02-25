521e0a10509763a8f8e25aadb2a613676cd22e5a
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2020-02-24 20:35:05 -0500

    Remove package not useful for this

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,8 +93,6 @@ LOGIN_URL = "account_login"
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
 PASSWORD_HASHERS = [
-    # https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
-    "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
     "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
