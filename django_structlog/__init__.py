04eda7da31c5d89cdd6ccf47043e595b65a528a2
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2022-08-01 20:46:28 -0400

    Bump version

--- django_structlog/__init__.py
+++ django_structlog/__init__.py
@@ -5,6 +5,6 @@
 default_app_config = "django_structlog.apps.DjangoStructLogConfig"
 name = "django_structlog"
 
-VERSION = (2, 2, 0)
+VERSION = (3, 0, 0)
 
 __version__ = ".".join(str(v) for v in VERSION)
