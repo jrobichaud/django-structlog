fc15ba7806735887348b96cab2c6d4f3fe7d6de8
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2022-10-22 10:57:33 -0400

    Remove overkill password validations for a local demo project

--- config/settings/base.py
+++ config/settings/base.py
@@ -99,14 +99,7 @@ PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.BCryptPasswordHasher",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-    },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
-]
+AUTH_PASSWORD_VALIDATORS = []
 
 # MIDDLEWARE
 # ------------------------------------------------------------------------------
