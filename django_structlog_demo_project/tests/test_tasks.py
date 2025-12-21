cb8ab2151f3cf8c57f3096148e1d5eac3ab0350a
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2025-12-21 08:43:57 -0500

    fix test

--- django_structlog_demo_project/tests/test_tasks.py
+++ django_structlog_demo_project/tests/test_tasks.py
@@ -1,3 +1,5 @@
+import logging
+
 import django
 import pytest
 
@@ -8,11 +10,14 @@ if django.VERSION >= (6, 0):
 
     class TestDjangoTask:
         def test(self, caplog):
-            task_result = tasks.django_task.enqueue()
-            assert task_result == "ok"
+            with caplog.at_level(
+                logging.INFO, logger="django_structlog_demo_project.tasks"
+            ):
+                task_result = tasks.django_task.enqueue()
+            assert task_result.return_value == "ok"
 
-            assert len(caplog.records) == 1
-            record = caplog.records[0]
+            assert len(caplog.records) == 3
+            record = caplog.records[1]
             assert (
                 record.msg["event"]
                 == "This is a Django 6 native task using the built-in task framework"
