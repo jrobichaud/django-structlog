5a88e6788e91e66c0e3364e12710728f11a988ee
Chris Rose <offline@offby1.net>
2026-04-26 09:05:16 -0700

    Make the log level for the request middleware fully configurable
    - adds `STATUS_DEFAULT_LOG_LEVEL`, `STATUS_START_LOG_LEVEL`, and `STATUS_2XX_LOG_LEVEL` app settings
    - adds `CELERY_DEFAULT_LOG_LEVEL`, `CELERY_TASK_START_LOG_LEVEL`, `CELERY_TASK_SUCCESS_LOG_LEVEL`, `CELERY_TASK_NOTICE_LOG_LEVEL`, `CELERY_TASK_FAILURE_LOG_LEVEL`, `CELERY_TASK_ERROR_LOG_LEVEL`
    - uses settings to determine the log level for all non-exception path logging in the request middleware
    
    Fixes #1022

--- django_structlog/app_settings.py
+++ django_structlog/app_settings.py
@@ -12,6 +12,50 @@ class AppSettings:
         return getattr(settings, self.PREFIX + "CELERY_ENABLED", False)
 
     @property
+    def CELERY_DEFAULT_LOG_LEVEL(self) -> int:
+        return getattr(settings, self.PREFIX + "CELERY_DEFAULT_LOG_LEVEL", logging.INFO)
+
+    @property
+    def CELERY_TASK_START_LOG_LEVEL(self) -> int:
+        return getattr(
+            settings,
+            self.PREFIX + "CELERY_TASK_START_LOG_LEVEL",
+            self.CELERY_DEFAULT_LOG_LEVEL,
+        )
+
+    @property
+    def CELERY_TASK_SUCCESS_LOG_LEVEL(self) -> int:
+        return getattr(
+            settings,
+            self.PREFIX + "CELERY_TASK_SUCCESS_LOG_LEVEL",
+            self.CELERY_DEFAULT_LOG_LEVEL,
+        )
+
+    @property
+    def CELERY_TASK_NOTICE_LOG_LEVEL(self) -> int:
+        return getattr(
+            settings,
+            self.PREFIX + "CELERY_TASK_RETRY_LOG_LEVEL",
+            logging.WARNING,
+        )
+
+    @property
+    def CELERY_TASK_FAILURE_LOG_LEVEL(self) -> int:
+        return getattr(
+            settings,
+            self.PREFIX + "CELERY_TASK_FAILURE_LOG_LEVEL",
+            logging.INFO,
+        )
+
+    @property
+    def CELERY_TASK_ERROR_LOG_LEVEL(self) -> int:
+        return getattr(
+            settings,
+            self.PREFIX + "CELERY_TASK_ERROR_LOG_LEVEL",
+            logging.ERROR,
+        )
+
+    @property
     def IP_LOGGING_ENABLED(self) -> bool:
         return getattr(settings, self.PREFIX + "IP_LOGGING_ENABLED", True)
 
@@ -19,6 +63,26 @@ class AppSettings:
     def REQUEST_CANCELLED_LOG_LEVEL(self) -> int:
         return getattr(
             settings, self.PREFIX + "REQUEST_CANCELLED_LOG_LEVEL", logging.WARNING
+        )
+
+    @property
+    def STATUS_DEFAULT_LOG_LEVEL(self) -> int:
+        return getattr(settings, self.PREFIX + "STATUS_DEFAULT_LOG_LEVEL", logging.INFO)
+
+    @property
+    def STATUS_START_LOG_LEVEL(self) -> int:
+        return getattr(
+            settings,
+            self.PREFIX + "STATUS_START_LOG_LEVEL",
+            self.STATUS_DEFAULT_LOG_LEVEL,
+        )
+
+    @property
+    def STATUS_2XX_LOG_LEVEL(self) -> int:
+        return getattr(
+            settings,
+            self.PREFIX + "STATUS_2XX_LOG_LEVEL",
+            self.STATUS_DEFAULT_LOG_LEVEL,
         )
 
     @property
